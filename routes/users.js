3a9b09a6bb2200bc0e0d46310e91ea06ad9fc792
Luay-NMT <132140798+Luay-NMT@users.noreply.github.com>
2023-05-05 16:28:52 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
     "LEFT JOIN `usergroups` ug ON u.userId = ug.userId " +
     "LEFT JOIN `groups` g ON ug.groupId = g.id";
       connenction.query(queryGetAllUsers, (err, result) => {
-        connenction.destroy();
+        connenction.end();
         if (err) {
           console.log(err);
         } else {
@@ -25,122 +25,122 @@ router.get("/", (req, res) => {
       });
     });
 
-    router.get("/:id", (req, res) => {
-      const id = req.params.id;
-      var connenction = db.dbConnect();
-      connenction.connect((err) => {
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  var connenction = db.dbConnect();
+  connenction.connect((err) => {
+    if (err) {
+      console.log(err);
+    }
+});
+    var queryGetUserBasedOnUserId = "SELECT u.userId, u.shortName, u.fullName, g.`group`, g.groupName " +
+    "FROM `users` u " +
+    "LEFT JOIN `usergroups` ug ON u.userId = ug.userId " +
+    "LEFT JOIN `groups` g ON ug.groupId = g.id WHERE u.userId = ?"
+      connenction.query(queryGetUserBasedOnUserId,[id], (err, result) => {
+        connenction.end();
         if (err) {
           console.log(err);
+        } else {
+          res.send(result);
         }
+      });
     });
-        var queryGetUserBasedOnUserId = "SELECT u.userId, u.shortName, u.fullName, g.`group`, g.groupName " +
-        "FROM `users` u " +
-        "LEFT JOIN `usergroups` ug ON u.userId = ug.userId " +
-        "LEFT JOIN `groups` g ON ug.groupId = g.id WHERE u.userId = ?"
-          connenction.query(queryGetUserBasedOnUserId,[id], (err, result) => {
-            connenction.destroy();
+
+router.post("/", (req, res) => {
+  const fullName = req.body.fullname;
+  const shortName = req.body.shortName;
+  const group = req.body.group;
+  var connenction = db.dbConnect();
+  connenction.connect((err) => {
+    if (err) {
+      console.log(err);
+    }
+});
+    var queryAddToUsers = "INSERT INTO users (shortName, fullName) VALUES (?, ?)";
+      connenction.query(queryAddToUsers, [shortName, fullName], (err, result) => {
+        
+        if (err) {
+          console.log(err);
+        }else {
+        
+          var queryAddToUserGroups = "INSERT INTO `usergroups` (userId, groupId) " +
+                        "VALUES (?, (SELECT id FROM `groups` WHERE `group` = ?));";
+          connenction.query(queryAddToUserGroups, [result.insertId, group], (err, result) => {
             if (err) {
               console.log(err);
-            } else {
-              res.send(result);
             }
-          });
-        });
-
-    router.post("/", (req, res) => {
-      const fullName = req.body.fullname;
-      const shortName = req.body.shortName;
-      const group = req.body.group;
-      var connenction = db.dbConnect();
-      connenction.connect((err) => {
-        if (err) {
-          console.log(err);
-        }
-    });
-        var queryAddToUsers = "INSERT INTO users (shortName, fullName) VALUES (?, ?)";
-          connenction.query(queryAddToUsers, [shortName, fullName], (err, result) => {
-            
-            if (err) {
-              console.log(err);
-            }else {
-            
-              var queryAddToUserGroups = "INSERT INTO `usergroups` (userId, groupId) " +
-                            "VALUES (?, (SELECT id FROM `groups` WHERE `group` = ?));";
-              connenction.query(queryAddToUserGroups, [result.insertId, group], (err, result) => {
-                if (err) {
-                  console.log(err);
-                }
-                });
-                res.send(result);
-              }
-              connenction.destroy();        
-
-          });
-        });
-
-
-
-
-      router.put("/", (req, res) => {
-      const fullName = req.body.fullname;
-      const shortName = req.body.shortName;
-      const groupId = req.body.groupId;
-      const userId = req.body.userId;
-          var connenction = db.dbConnect();
-          connenction.connect((err) => {
-            if (err) { console.log(err); }
-        });
-
-          var queryUpdateUser = "UPDATE users SET shortName = ?, fullName = ?, createdDate = NOW() WHERE userId = ?";
-    
-          
-          
-          connenction.query(queryUpdateUser, [shortName, fullName, userId], (err, result) => {
-            if (err) { console.log(err); } else {
-            var queryUpdateUserGroup = "UPDATE usergroups SET groupId = ? WHERE userId = ?";
-            connenction.query(queryUpdateUserGroup, [groupId, userId], (err, result) => {
-            connenction.destroy();
-              if (err) { console.log(err); }
             });
             res.send(result);
           }
+          connenction.end();        
 
-          });
-
-        });
-
-        router.delete('/delete/:id', (req, res) => {
-          var connenction = db.dbConnect();
-          const userId = req.params.id;
-
-          connenction.connect((err) => {
-            if (err) { console.log(err); }
-        });
-        var queryDeleteFromBookings= "DELETE FROM `bookings` WHERE userId = ?";
-        connenction.query(queryDeleteFromBookings, userId, (err, result) => {
-
-          if (err) { console.log(err); }else {
-            
-          
-        var queryDeleteFromUserGroups = "DELETE FROM `usergroups` WHERE userId = ?";
-          connenction.query(queryDeleteFromUserGroups, userId, (err, result) => {
-
-            if (err) { console.log(err); }
-            var queryDeleteFromUsers = "DELETE FROM `users` WHERE userId = ?";
-            connenction.query(queryDeleteFromUsers, userId, (err, result) => {
-              connenction.destroy();
-              if (err) { console.log(err); }
+      });
+    });
 
 
-          });
-        
+
+
+  router.put("/", (req, res) => {
+  const fullName = req.body.fullname;
+  const shortName = req.body.shortName;
+  const groupId = req.body.groupId;
+  const userId = req.body.userId;
+      var connenction = db.dbConnect();
+      connenction.connect((err) => {
+        if (err) { console.log(err); }
+    });
+
+      var queryUpdateUser = "UPDATE users SET shortName = ?, fullName = ?, createdDate = NOW() WHERE userId = ?";
+
+      
+      
+      connenction.query(queryUpdateUser, [shortName, fullName, userId], (err, result) => {
+        if (err) { console.log(err); } else {
+        var queryUpdateUserGroup = "UPDATE usergroups SET groupId = ? WHERE userId = ?";
+        connenction.query(queryUpdateUserGroup, [groupId, userId], (err, result) => {
+        connenction.end();
+          if (err) { console.log(err); }
         });
         res.send(result);
-
       }
-      });
 
       });
+
+    });
+
+    router.delete('/delete/:id', (req, res) => {
+      var connenction = db.dbConnect();
+      const userId = req.params.id;
+
+      connenction.connect((err) => {
+        if (err) { console.log(err); }
+    });
+    var queryDeleteFromBookings= "DELETE FROM `bookings` WHERE userId = ?";
+    connenction.query(queryDeleteFromBookings, userId, (err, result) => {
+
+      if (err) { console.log(err); }else {
+        
+      
+    var queryDeleteFromUserGroups = "DELETE FROM `usergroups` WHERE userId = ?";
+      connenction.query(queryDeleteFromUserGroups, userId, (err, result) => {
+
+        if (err) { console.log(err); }
+        var queryDeleteFromUsers = "DELETE FROM `users` WHERE userId = ?";
+        connenction.query(queryDeleteFromUsers, userId, (err, result) => {
+          connenction.end();
+          if (err) { console.log(err); }
+
+
+      });
+    
+    });
+    res.send(result);
+
+  }
+  });
+
+  });
 
 
 
